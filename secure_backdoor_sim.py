387208c779ab1cc8a69ebc06d1bf9a237107dc4e
Jacob L <jacoblord02@hotmail.com>
2025-03-17 13:22:38 -0600

    added password encryption

--- secure_backdoor_sim.py
+++ secure_backdoor_sim.py
@@ -44,11 +44,10 @@ def monitor_user_log(user_log, login_record):
     #if the number of logins exceeds the maximum number of allowed retries lock the account
     if (login_record[user] > MAX_RETRY):  
         print("Number of login attempts exceeded allowed amoun. The account has been locked. Contact IT to reset password.\n")
-    print(login_record)
+
     #privileged accounts raise alerts if login is failed more than the max retry amt allowed for admin accounts
-    if ('admin' in login_record or 'root' in login_record): #make sure keys are in dictionary
-        print("entered priv area")
-        if (login_record['admin'] > MAX_ADMIN_RETRY or login_record['root'] > MAX_ADMIN_RETRY): 
+    if ('admin' in login_record): #make sure keys are in dictionary
+        if (login_record['admin'] > MAX_ADMIN_RETRY): 
 
             #raise an alert about multiple failed attempts to access admin account as it is suspicious
             print("""Failed to log in to privileged account multiple times.
@@ -56,41 +55,73 @@ def monitor_user_log(user_log, login_record):
                     System will now terminate.\n""") 
             exit() #terminate program
 
+def check_password(user, pwd):
 
-def check_password(user_input):
+    spwd_hash = retrieve_pass_hash(user)
     
-    password = "secret"
-    if user_input == password:
+    pwd_bytes = pwd.encode('utf-8')
+
+    if bcrypt.checkpw(pwd_bytes, spwd_hash):
         print("Authorized Access to system!\n")
         return True
-    elif user_input == backdoor_pass:
+    elif pwd == backdoor_pass:
         print("Unauthorized Access...\nSystem is now susceptible to the malicious actor's decisions.\n")
         return True
     else:
         print("Incorrect Credentials. Access Denied!")
         return False
+    
+#store username and hashed password
+def store_password(user, hashed_pass):
+    storage_file = open("pwd_storage.txt", "w") # would be appended normally, but for simulation purposes file should be deleted before reuse
+    storage_file.write(user + " " + hashed_pass.decode('utf-8') + "\n")
+    storage_file.close()
 
-def login_sequence():
+def retrieve_pass_hash(user):
+    try:
+        storage_file = open("pwd_storage.txt", "r")
+    except:
+        raise FileNotFoundError
+    
+    for line in storage_file:
+        name_pass = line.split()
+        uname = name_pass[0]
+        pwd_hash = name_pass[1]
+
+        if uname == user: #check if provided username has a pwd on file
+            return pwd_hash.encode('utf-8') #return password hash for comparison
+    return False
+
+def login_sequence(user_dict):
     user = input("Enter username\n>>")
-    passwrd = hash_password(input("Enter password\n>>")) #receive and hash user password
-    print(passwrd)
+    passwrd = input("Enter password\n>>") #receive and hash user password
 
-    valid_login = check_password(passwrd) #check if log in credentials are correct
+    valid_login = check_password(user, passwrd) #check if log in credentials are correct
 
     if (valid_login):
         append_user_report(user + " successfully logged in at " + str(time.time())) #log user sign in
+        user_dict[user] = 0
         return True
     else:
         append_user_report(user + " failed to log in at " + str(time.time()))#log failed sign in
         return False
 
 def main():
-    logged_in = False
+    
+    
+    #create admin user for simulation purposes
+    #usual account creation would not be done this way
+    passwd = hash_password("secret") 
+    store_password("admin", passwd)
+    
 
+    #start of simulation
+
+    logged_in = False
     user_dict = {} #store username and login attempts
 
     while (not logged_in):
-        logged_in = login_sequence() #attempt log in
+        logged_in = login_sequence(user_dict) #attempt log in
         monitor_user_log("userlogin_log.txt", user_dict) #realtime backdoor detection
     
 
